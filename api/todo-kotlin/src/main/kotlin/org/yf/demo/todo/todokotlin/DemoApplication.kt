49136a860e7e3294166c23545ccb485f52c2fc18
Yonatan Fessehaye <fessehayeyonatan@gmail.com>
2019-12-19 22:55:11 +0100

    maven init

---
+++ api/todo-kotlin/src/main/kotlin/org/yf/demo/todo/todokotlin/DemoApplication.kt
@@ -0,0 +1,11 @@
+package org.yf.demo.todo.todokotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
---
+++ api/todo-kotlin/src/test/kotlin/org/yf/demo/todo/todokotlin/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.yf.demo.todo.todokotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
